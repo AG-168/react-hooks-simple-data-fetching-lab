6589dd61cc33a5925c2d9f6b89341e1eab9835c6
Anson Guo <anson.guo.921104@gmail.com>
2023-02-24 15:32:55 -0500

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+
+import React , { useEffect, useState } from "react"
+
+function App () {
+
+    const [imageUrl, setImageUrl] = useState(null)
+
+    useEffect(()=> {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res=>res.json())
+        .then(data=>setImageUrl(data.message))
+        //.then(data=>console.log(data))
+    }, [])
+
+    if (!imageUrl) {return <p>Loading...</p>}
+
+    return <img src={imageUrl} alt="A Ramdom Dog"/>
+
+}
+
+export default App
